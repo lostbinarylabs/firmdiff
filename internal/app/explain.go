bbd244f0614d23dd8a8fcf39630b4640a557ba98
paulclarke <paul.clarke@rfi.com.au>
2026-03-11 11:18:34 +1030

    Add `--top` flag to `explain` command for customizable symbol table size

--- internal/app/explain.go
+++ internal/app/explain.go
@@ -8,25 +8,24 @@ import (
 
 // NewExplainCmd returns the explain command for comparing two ELF files.
 func NewExplainCmd() *cobra.Command {
+	var top int
 
 	cmd := &cobra.Command{
 		Use:     "explain <A.elf> <B.elf>",
 		Short:   "Explain why firmware size changed",
 		GroupID: "core",
 		Args:    cobra.ExactArgs(2),
-		RunE: func(_ *cobra.Command, args []string) error {
-
-			aPath := args[0]
-			bPath := args[1]
-
-			return runExplain(aPath, bPath)
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return runExplain(args[0], args[1], top)
 		},
 	}
+
+	cmd.Flags().IntVar(&top, "top", 10, "number of symbols to show in growth/shrink tables")
 
 	return cmd
 }
 
-func runExplain(aPath, bPath string) error {
+func runExplain(aPath, bPath string, top int) error {
 
 	aRes, err := AnalyzeELF(aPath, 500)
 	if err != nil {
@@ -44,7 +43,7 @@ func runExplain(aPath, bPath string) error {
 	fmt.Printf("FLASH delta: %+d bytes\n", deltaFlash)
 	fmt.Println()
 
-	grown, shrunk := DiffSymbols(aRes.TopSyms, bRes.TopSyms, 10)
+	grown, shrunk := DiffSymbols(aRes.TopSyms, bRes.TopSyms, top)
 
 	if len(grown) > 0 {
 		fmt.Println("Top causes of growth")
